ee81d50f1c30ccca7d3e671e3766008ffdbdd32c
Max Seavey <58485607+cvmaxie@users.noreply.github.com>
2022-11-22 00:02:40 -0500

    aberration overlay is back in gamecanvas

--- assets/scripts/gamecanvas.gd
+++ assets/scripts/gamecanvas.gd
@@ -19,19 +19,39 @@ var currentRoomIndex = 0
 export var camera_pixel_width : int = 320
 export var camera_pixel_height : int = 180
 
+var aberration_position = Vector2(0, 0)
+var abrpos
+
+var aberrationTimer
+
+export var aberration_timer_interval : float = 1.0
+export var aberration_intensity : int = 1
+export var aberration_speed : float = 0.5
+
+var aberration_overlay_material
+
+
 
 func _ready():
 	
 	Globals.planeManager = $ViewportContainer/Viewport/Room/PlaneManager
 	Globals.party = $ViewportContainer/Viewport/Room/PlaneManager/Overworld/PARTY
 	Globals.soundManager = $SoundManager
-	
+
 	Globals.dialogueBox = $UserInterface/ReferenceRect/DialogueBox
 	Globals.portrait = $UserInterface/ReferenceRect/Portraits
 	Globals.colorManager = $UserInterface/ReferenceRect/DialogueBox/ColorManager
 	
 	camera.rescale_camera(floor(OS.window_size.x/camera_pixel_width))
 	set_current_room(rooms[0])
+	
+	aberrationTimer = Timer.new()
+	add_child(aberrationTimer)
+	aberrationTimer.wait_time = aberration_timer_interval
+	aberrationTimer.connect("timeout", self, "_on_timer_timeout")
+	aberrationTimer.start()
+	
+	aberration_overlay_material = $MotionOverlay.material
 
 
 func _process(delta):
@@ -43,7 +63,19 @@ func _process(delta):
 		
 	if Input.is_action_just_pressed("reset") && Globals.mode == Enums.Mode.WALK:
 		reset_game()
+	
+	var aberrationOverlay = lerp(aberration_overlay_material.get_shader_param("r_displacement"), aberration_position, aberration_speed)	
+	
+	aberration_overlay_material.set_shader_param("r_displacement", aberrationOverlay)
 
+func _on_timer_timeout():
+	var abrx = rand_range(-aberration_intensity, aberration_intensity)
+	var abry = rand_range(-aberration_intensity, aberration_intensity)
+	
+	aberration_position = Vector2(abrx, abry)
+	
+	
+	print("second")
 
 func reset_game():
 	
